b4f37bc3273be474e6c0ac8b275458db230ee0f6
Dzoel31 <dzulfikriadjmal@gmail.com>
2023-04-27 15:04:34 +0700

    Menambahkan fungsi insert data

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 import mysql.connector
 import os
 
-try :
+try:
     conn = mysql.connector.connect(
         user="root",
         password="",
     )
-    
+
 except mysql.connector.Error as e:
     print(f"Terjadi error pada program : {e}")
+    print("Silahkan jalankan ulang program")
     os.system("C:\\xampp\\xampp-control.exe")
 
 cur = conn.cursor()
 
+
 def queryInsert():
-    pass
+    repeat = "y"
+    while repeat != "n":
+        showTable()
+        print("="*20)
+        print("[1] Pilih table")
+        print("[2] Exit")
+        print("="*20)
+        choose = int(input("Masukkan pilihan : "))
+
+        if choose == 1:
+            table = input("Pilih table : ")
+            cur.execute(f"DESC {table}")
+            field = [f[0] for f in cur]
+            value = []
+
+            for i in range(len(field)):
+                value.append(input(f"Masukkan {field[i]} : "))
+            
+            sqlInsertQuery = f"""INSERT INTO {table} VALUES {tuple(value)}"""
+
+            cur.execute(sqlInsertQuery)
+            conn.commit()
+        elif choose == 2:
+            break
+        else:
+            input("Input tidak sesuai!\nEnter untuk mengulang!")
+            mainMenu()
+
+        repeat = input("\nIngin melanjutkan? [Y/N or Any key] : ").lower()
+
 
 def showTable():
     os.system("cls")
@@ -23,20 +54,38 @@ def showTable():
     listTable = ["".join(x) for x in cur]
 
     print("="*40)
-    print("|{:38}|".format("TABEL"))
+    print("|{:^38}|".format("TABEL"))
     print("="*40)
     for i in range(len(listTable)):
         print("|{:^38}|".format(listTable[i]))
     print("="*40)
 
-def querySelect():
-    showTable()
-    choose1 = input("Pilih table : ")
-    cur.execute(f"SELECT * FROM {choose1}")
-    conTable = [list(x) for x in cur]
 
-    for i in range(len(conTable)):
-        print(conTable[i])
+def querySelect():
+    repeat = "y"
+    while repeat != "n":
+        showTable()
+        print("="*20)
+        print("[1] Pilih table")
+        print("[2] Exit")
+        print("="*20)
+        choose = int(input("Masukkan pilihan : "))
+
+        if choose == 1:
+            table = input("Pilih table : ")
+            cur.execute(f"SELECT * FROM {table}")
+            conTable = [list(x) for x in cur]
+
+            for i in range(len(conTable)):
+                print(conTable[i])
+        elif choose == 2:
+            break
+        else:
+            input("Input tidak sesuai!\nEnter untuk mengulang!")
+            mainMenu()
+
+        repeat = input("\nIngin melanjutkan? [Y/N or Any key] : ").lower()
+
 
 def selectDatabase():
     os.system("cls")
@@ -52,42 +101,37 @@ def selectDatabase():
 
     choose = input("Pilih database : ")
     cur.execute(f"USE {choose}")
-    useDatabase()
+
 
 def useDatabase():
     print("Database saat ini :", conn.database)
 
+
 def mainMenu():
     os.system("cls")
+    useDatabase()
     print("="*20)
     print("[1] Pilih database")
-    print("[2] Exit")
+    print("[2] Query select")
+    print("[3] Query insert")
+    print("[0] Exit")
     print("="*20)
 
     choose = int(input("Masukkan pilihan : "))
     if choose == 1:
         selectDatabase()
+        mainMenu()
     elif choose == 2:
-        exit()
-    else :
-        input("Input tidak sesuai!\n Tekan enter untuk lanjut!")
-    
-    showTable()
-
-    print("="*20)
-    print("[1] Pilih table")
-    print("[2] Exit")
-    print("="*20)
-
-    choose1 = int(input("Masukkan pilihan : "))
-    if choose1 == 1:
         querySelect()
-    elif choose1 == 2:
+        mainMenu()
+    elif choose == 3:
+        queryInsert()
+        mainMenu()
+    elif choose == 0:
         exit()
-    else :
+    else:
         input("Input tidak sesuai!\n Tekan enter untuk lanjut!")
-
-    querySelect()
+        mainMenu()
 
 
 mainMenu()
